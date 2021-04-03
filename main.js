428ad817dbadd4c891a824ec0364fa7fa9cc6287
gerald-tan <geraldtan97@gmail.com>
2021-04-03 13:22:49 +0800

    Fixed game canvas/modal

--- main.js
+++ main.js
@@ -239,33 +239,3 @@ function sum(arr, stop=arr.length) {
     arr = arr.slice(0, stop)
     return arr.reduce((a, b) => a + b, 0);
 }
-
-
-// For game
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
