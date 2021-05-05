7776cb70dcd3626229ed7433769068fbefaaf977
tanweilee2@gmail.com <tanweilee2@gmail.com>
2021-05-05 17:32:08 +0800

    Preload testing rev 1

--- d059c1812c7d78f2653bd748c878ea8aa6ea9004/js/Sizing.js
+++ d059c1812c7d78f2653bd748c878ea8aa6ea9004/js/Sizing.js
@@ -694,6 +694,13 @@ window.onclick = function(event) {
     }
 }
 
-
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
 
 
