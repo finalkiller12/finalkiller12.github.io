1b0b7631288b72e419bf4d4629497aa6a1b0e6c1
tanweilee2@gmail.com <tanweilee2@gmail.com>
2021-05-06 17:34:51 +0800

    PDF working liao

--- d059c1812c7d78f2653bd748c878ea8aa6ea9004/js/Sizing.js
+++ d059c1812c7d78f2653bd748c878ea8aa6ea9004/js/Sizing.js
@@ -646,15 +646,15 @@ startTour();
 
 // Get the modal Results
 var modal = document.getElementById("myModal");
-//var modal2 = document.getElementById("catModal");
+var modal2 = document.getElementById("catModal");
 
 // Get the button that opens the modal
 var result = document.getElementById("result-Btn");
-//var catalogue = document.getElementById("catalogue-Btn");
+var catalogue = document.getElementById("catalogue-Btn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-//var span2 = document.getElementsByClassName("close")[1];
+var span2 = document.getElementsByClassName("close")[1];
 
 // When the user clicks the button, open the modal 
 let isFirstModalClick = true;
@@ -673,26 +673,26 @@ result.onclick = function() {
 }
 
 // When the user clicks the button, open the modal 
-//catalogue.onclick = function() {
-//    modal2.style.display = "block";
-//}
+catalogue.onclick = function() {
+    modal2.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-//span2.onclick = function() {
-//    modal2.style.display = "none";
-//  }
+span2.onclick = function() {
+    modal2.style.display = "none";
+  }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal)  {
     modal.style.display = "none";
     }
-//  if (event.target == modal2) {
-//    modal2.style.display = "none";
-//   }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+   }
 }
 
   jQuery(function($){
